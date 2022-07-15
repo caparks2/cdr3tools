bd01b01c2644e531348f001e23d7eca6d7967367
Christopher Parks <caparks2@gmail.com>
2022-07-15 16:40:37 -0400

    fix IMGT fasta headers

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' IMGT Human TRBV Reference Sequences
+#'
+#' A file containing the IMGT Human TRBV reference set
+#'   used in IMGT/V-QUEST.
+#'
+#' @format FASTA formatted text file.
+#'
+#' @source \url{https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/TR/TRBV.fasta}
+#'
+"vgenes"
