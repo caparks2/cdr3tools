7386bbff99e352270d280a1b1df6b866a3edc88c
Christopher Parks <caparks2@gmail.com>
2022-08-17 23:14:23 -0400

    added format_immunarch and collapse_sequences

--- R/collapse_sequences.R
+++ R/collapse_sequences.R
@@ -1,7 +1,17 @@
 #' Collapse Unique Sequences
 #'
 #' Collapse repertoires using various combinations of unique sequence
-#'   definitions
+#'   definitions.
+#'
+#' Rearrangement sequences are grouped according to the user selected unique
+#'   sequence definition and returned as list columns. The returned data will
+#'   contain one row per unique sequence. A column `duplicates` is added to
+#'   show the number of rearrangement sequences that match to a single
+#'   unique sequence. This is useful for minimizing PCR and sequencing errors that
+#'   result in artificially inflated unique sequence counts.
+#'
+#' @param data A list or a single data frame as returned by
+#'   [cdr3tools::read_immunoseq].
 #' @param .group_by Character. The combination of T cell receptor sequences and
 #'   V and J genes to use in order to group unique sequences. If `NULL`, the
 #'   default is to group unique sequences by CDR3 nucleotide sequence plus the
@@ -11,86 +21,66 @@
 #'       \item{"ntvj"}{*Default*. CDR3 nucleotide sequence plus most resolved V and J gene calls}
 #'       \item{"aa"}{CDR3 amino acid sequence}
 #'       \item{"aavj"}{CDR3 amino acid sequence plus most resolved V and J gene calls}
-#'       \item{"rearrangement"}{full nucleotide sequence (not suitable for comparisons across platforms)}
-#'       \item{"extended_rearrangement"}{extended full nucleotide sequence (not suitable for comparisons across platforms)}
-#'       \item{"rearrangement_trunc"}{truncated full nucleotide sequence}
 #'     }
-#' @param .use_reads Logical. `FALSE` (the default) indicates that repertoire
-#'   frequencies should be calculated using templates. `TRUE` indicates that
-#'   repertoire frequencies should be calculated using reads. Immunoseq files
-#'   are sometimes exported without reads information if templates are present
-#'   in the data. In this case `` will throw an
-#'   error if `.use_reads` = TRUE.
-#' @name collapse_sequences
-NULL
-# if (is.null(.use_reads)) {
-#   .use_reads <- FALSE
-# }
-# if (.use_reads) {
-#   purrr::map(data, function(data) {
-#     if (all(is.na(data$seq_reads))) {
-#       rlang::abort("data$seq_reads has no values. Set .use_reads = FALSE to use templates.")
-#     }
-#   })
-# }
-#
-# if (!is.null(.group_by)) {
-#
-#   .group_by <- rlang::arg_match(.group_by,
-#                                 c(
-#                                   "nt", "ntvj", "aa", "aavj",
-#                                   "rearrangement",
-#                                   "rearrangement_trunc",
-#                                   "extended_rearrangement"
-#                                 )
-#   )
-#   .group_by <- switch(.group_by,
-#                       nt                     = "cdr3_rearrangement",
-#                       ntvj                   = c("cdr3_rearrangement", "v_resolved", "j_resolved"),
-#                       aa                     = "cdr3_amino_acid",
-#                       aavj                   = c("cdr3_amino_acid", "v_resolved", "j_resolved"),
-#                       rearrangement          = "rearrangement",
-#                       rearrangement_trunc    = "rearrangement_trunc",
-#                       extended_rearrangement = "extended_rearrangement"
-#   )
-#   data <- purrr::map(data, function(data) {
-#     grouped_data <- data %>%
-#       dplyr::group_by(dplyr::across(tidyselect::all_of({{ .group_by }}))) %>%
-#       dplyr::summarise(templates = sum(.data$templates),
-#                        seq_reads = sum(.data$seq_reads)) %>%
-#       dplyr::ungroup()
-#     data <- data %>%
-#       dplyr::select(-templates, -seq_reads, -rearrangement, -rearrangement_trunc, -extended_rearrangement) %>%
-#       dplyr::inner_join(grouped_data, by = {{ .group_by }})
-#     data
-#   }
-#   )
-# }
-#
-# return(data)
-#
-# if (.use_reads) {
-#   data <- purrr::map(data, function(x) {
-#     x %>%
-#       # add a seq_read frequency column
-#       dplyr::mutate(
-#         frequency = .data$seq_reads / sum(.data$seq_reads),
-#         .after = .data$seq_reads
-#       ) %>%
-#       # sort by frequency
-#       dplyr::arrange(dplyr::desc(.data$frequency))
-#   }
-#   )
-# } else {
-#   data <- purrr::map(data, function(x) {
-#     x %>%
-#       # add a template frequency column
-#       dplyr::mutate(
-#         frequency = .data$templates / sum(.data$templates),
-#         .after = .data$templates
-#       ) %>%
-#       # sort by frequency
-#       dplyr::arrange(dplyr::desc(.data$frequency))
-#   }
-#   )
-# }
+#' @returns A list or a single data frame of Immunoseq data with
+#' @examples
+#' collapse_sequences(cfselo_seqs)
+#' @author Christopher Parks
+#' @export
+collapse_sequences <- function(data, .group_by = NULL) {
+  if (!inherits(data, "list") && inherits(data, "data.frame")) {
+    data <- list(data)
+  }
+
+  if (is.null(.group_by)) .group_by <- "ntvj"
+
+  .group_by <- rlang::arg_match(.group_by, c("nt", "ntvj", "aa", "aavj"))
+
+  .group_by <- switch(.group_by,
+    "nt"   = c("cdr3_rearrangement"),
+    "ntvj" = c("cdr3_rearrangement", "v_resolved", "j_resolved"),
+    "aa"   = c("cdr3_amino_acid"),
+    "aavj" = c("cdr3_amino_acid", "v_resolved", "j_resolved")
+  )
+
+  data <- purrr::map(data, ~ .x %>%
+    dplyr::group_by(
+      dplyr::across(
+        c(tidyselect::all_of(.group_by), sample_name, frame_type)
+      )
+    ) %>%
+    dplyr::summarise(
+      duplicates = dplyr::n(),
+      templates = sum(templates),
+      seq_reads = sum(seq_reads),
+      cdr3_rearrangement = cdr3_rearrangement[1],
+      cdr3_amino_acid = cdr3_amino_acid[1],
+      v_resolved = v_resolved[1],
+      d_resolved = d_resolved[1],
+      j_resolved = j_resolved[1],
+      n1_index = n1_index[1],
+      v_index = v_index[1],
+      d_index = d_index[1],
+      n2_index = n2_index[1],
+      j_index = j_index[1],
+      n1_insertions = n1_insertions[1],
+      n2_insertions = n2_insertions[1],
+      rearrangement = list(rearrangement),
+      rearrangement_trunc = list(rearrangement_trunc),
+      .groups = "drop"
+    ) %>%
+    dplyr::mutate(frequency = templates / sum(templates)) %>%
+    dplyr::arrange(dplyr::desc(frequency)) %>%
+    dplyr::select(
+      sample_name, frame_type, duplicates, templates, frequency, seq_reads,
+      cdr3_rearrangement, cdr3_amino_acid, v_resolved, d_resolved,
+      j_resolved, n1_index, v_index, d_index, n2_index, j_index,
+      n1_insertions, n2_insertions, rearrangement, rearrangement_trunc
+    ))
+
+  if (length(data) == 1) {
+    return(data[[1]])
+  } else {
+    return(data)
+  }
+}
--- R/format_immunarch.R
+++ R/format_immunarch.R
@@ -1,5 +1,75 @@
-#' Format Repertoire Data for use with Immunarch
+#' Format Imunoseq Data for use with Immunarch
 #'
-format_immunarch <- function() {
+#' Prepare Immunoseq data according to Immunarch data format
+#'
+#' Works on Immunoseq data as returned by the [cdr3tools::read_immunoseq]
+#'   function. If unique sequences were collapsed using
+#'   [cdr3tools::collapse_sequences] the resulting column `sequence` will be a
+#'   list column containing the rearrangment sequences from each of the
+#'   duplicate unique sequences.
+#' @param .data A list or a single data frame as returned by
+#'   [cdr3tools::read_immunoseq].
+#' @returns A list or a single data frame of Immunarch formatted data.
+#' @author Christopher Parks
+#' @references \url{https://immunarch.com/reference/immunr_data_format.html}
+#' @examples
+#' format_immunarch(cfselo_seqs)
+#' @export
+format_immunarch <- function(.data) {
+  if (!inherits(.data, "list") && inherits(.data, "data.frame")) {
+    .data <- list(.data)
+  }
+  # Format immunoseq files to immunarch format
+  immdata <- purrr::map(.data, ~ .x %>%
+    # filter for in-frame, productive sequences
+    # filter(frame_type == 'In') %>%
+    # define CDR3 positions the immunarch way
+    dplyr::mutate(
+      V.end = n1_index - v_index,
+      D.start = d_index - v_index,
+      D.end = n2_index - v_index,
+      J.start = j_index - v_index
+    ) %>%
+    # where a region isn't defined, replace the value with NA (validated this with IMGT V-Quest)
+    dplyr::mutate(
+      dplyr::across(
+        .cols = c(V.end, D.start, D.end, J.start),
+        .fns = ~ dplyr::case_when(
+          .x < 0 ~ NA_real_,
+          TRUE ~ .x
+        )
+      )
+    ) %>%
+    # select the columns needed for immunarch format. Rename then as we go.
+    dplyr::select(
+      Clones = templates,
+      CDR3.nt = cdr3_rearrangement,
+      CDR3.aa = cdr3_amino_acid,
+      V.name = v_resolved,
+      D.name = d_resolved,
+      J.name = j_resolved,
+      V.end,
+      D.start,
+      D.end,
+      J.start,
+      VD.ins = n1_insertions,
+      DJ.ins = n2_insertions,
+      Sequence = rearrangement
+    ) %>%
+    # calculate proportion
+    dplyr::mutate(Proportion = Clones / sum(Clones), .after = Clones) %>%
+    # set V-J insertions to NA by definition since these are TRB sequences
+    dplyr::mutate(VJ.ins = NA, .after = J.start) %>%
+    # convert immunoseq VDJ gene names to IMGT gene names
+    dplyr::mutate(V.name = cdr3tools::imgt_format_gene_names(V.name)) %>%
+    dplyr::mutate(D.name = cdr3tools::imgt_format_gene_names(D.name)) %>%
+    dplyr::mutate(J.name = cdr3tools::imgt_format_gene_names(J.name)) %>%
+    # sort the data according to abundance
+    dplyr::arrange(desc(Proportion)))
 
+  if (length(immdata) == 1) {
+    return(immdata[[1]])
+  } else {
+    return(immdata)
+  }
 }
